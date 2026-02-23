230da7623510b42666a1033ab5d4bf1f44e5956b
Dydex <supremeajala@gmail.com>
2026-02-23 21:51:57 +0100

    updated contract

--- day-1/classwork/PropertyManagement/script/PropertyManagement.s.sol
+++ day-1/classwork/PropertyManagement/script/PropertyManagement.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {Script} from "forge-std/Script.sol";
 import {PropertyManagement} from "../src/PropertyManagement.sol";
 
-contract PropertyMangement is Script {
+contract PropertyManagement is Script {
     PropertyManagement public propertymangement;
 
     function setUp() public {}
